67125cdbe719c781c632458fddb5e224ab4b63ec
Marc Guinea <develop.marcguinea@gmail.com>
2017-09-12 15:32:47 +0200

    Removed unused scripts

--- src/components/check-wall.js
+++
@@ -1,9 +0,0 @@
-AFRAME.registerComponent('check-wall', {
-	init: function () {
-
-	},
-
-	tick: function (t, dt) {
-		
-	}
-});
--- src/components/collider-check-cookie.js
+++
@@ -1,7 +0,0 @@
-AFRAME.registerComponent('collider-check-cookie', {
-    init: function () {
-        this.el.addEventListener('collide', function (evt) {
-            console.log('This A-Frame entity collided with another entity!');
-        });
-    }
-});
